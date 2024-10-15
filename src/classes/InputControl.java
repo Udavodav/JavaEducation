6e4d4a2f8c6ee449d03a17d3ec2ebc14e362c9de
Виталий Коткин <vkotkin2002@mail.ru>
2024-10-15 22:36:56 +0400

    lesson2

---
+++ src/classes/InputControl.java
@@ -0,0 +1,29 @@
+package classes;
+
+public class InputControl {
+
+    public static boolean checkInput(String login, String password, String confirmPassword) {
+        String regex = "\\w+";
+
+        try {
+            if (!login.matches(regex) || login.length() >= 20) {
+                throw new WrongLoginException("Login must be at least 20 characters, must be contain only alphanumeric characters and underline");
+            }
+
+            if (!password.matches(regex) || password.length() >= 20 || !password.equals(confirmPassword)) {
+                throw new WrongPasswordException("Password must be at least 20 characters, must be contain only alphanumeric characters" +
+                        " and underline. Password and confirm password must be equals");
+            }
+        }
+        catch (WrongLoginException e) {
+            System.out.println(e.getMessage());
+            return false;
+        }
+        catch (WrongPasswordException e) {
+            System.out.println(e.getMessage());
+            return false;
+        }
+
+        return true;
+    }
+}
