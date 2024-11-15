e6577df22f78d6b927e0eb0a914c1dd5960c36c3
Виталий Коткин <vkotkin2002@mail.ru>
2024-11-15 17:13:38 +0400

    lesson4

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,6 @@ import classes.Student;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
--- src/classes/Student.java
+++ src/classes/Student.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Student {
+
     private String name;
     private String group;
     private int course;
